ddb1319005b1664d7f32c2b28919e61df8fd3718
Kira Hartlage <kira.hartlage@gmail.com>
2022-01-27 19:08:47 -0500

    get user's choice

--- PracticeApp/Program.cs
+++ PracticeApp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PracticeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // show user a menu
+            // offer options to user
+            // ask for their choice
+            // do their choice
+            // go back to menu
+            // go back to beginning
+            // unless choice is exit
+            // exit application
+            Console.WriteLine("Main Menu:");
+            Console.WriteLine("1. Add two numbers");
+            Console.WriteLine("2. Subtract two numbers");
+            Console.WriteLine("3. Remove whitespace from a string");
+            Console.WriteLine("4. Reverse a string");
+
+            Console.WriteLine("Enter your choice: ");
+            char userChoice = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("\n");
+            Console.WriteLine($"Your choice was {userChoice}");
+
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            Console.WriteLine("Program exited");
+        }
+    }
+}
