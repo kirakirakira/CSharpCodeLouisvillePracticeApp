baaa97e63f39fc19dc6f4cd5d18b066e4dd42619
Kira Hartlage <kira.hartlage@gmail.com>
2022-01-27 19:43:00 -0500

    some options

--- PracticeApp/Program.cs
+++ PracticeApp/Program.cs
@@ -4,6 +4,65 @@ namespace PracticeApp
 {
     internal class Program
     {
+        static void ShowMenu()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("Main Menu:");
+            Console.WriteLine("1. Add two numbers");
+            Console.WriteLine("2. Subtract two numbers");
+            Console.WriteLine("3. Remove whitespace from a string");
+            Console.WriteLine("4. Reverse a string");
+            Console.WriteLine("5. Quit");
+
+            Console.WriteLine("Enter your choice: ");
+            int userChoice = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+
+            Console.WriteLine("\n");
+            Console.WriteLine($"Your choice was {userChoice}");
+
+            switch (userChoice)
+            {
+                case 1:
+                    AddTwoNumbers();
+                    ShowMenu();
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey();
+                    Console.WriteLine("Program exited");
+                    break;
+
+                default:
+                    Console.WriteLine("You failed to make a valid choice, too bad!");
+                    Console.WriteLine("Just kidding, try again");
+                    ShowMenu();
+                    break;
+            }
+        }
+        static void AddTwoNumbers()
+        {
+            Console.WriteLine("You're adding two numbers");
+            Console.WriteLine("Enter the first number");
+            int first = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Enter the second number");
+            int second = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+            Console.WriteLine("\n");
+
+            Console.WriteLine($"The sum of {first} and {second} is {first + second}");
+
+        }
         static void Main(string[] args)
         {
             // show user a menu
@@ -14,23 +73,7 @@ namespace PracticeApp
             // go back to beginning
             // unless choice is exit
             // exit application
-            Console.WriteLine("Main Menu:");
-            Console.WriteLine("1. Add two numbers");
-            Console.WriteLine("2. Subtract two numbers");
-            Console.WriteLine("3. Remove whitespace from a string");
-            Console.WriteLine("4. Reverse a string");
-
-            Console.WriteLine("Enter your choice: ");
-            char userChoice = Console.ReadKey().KeyChar;
-
-            Console.WriteLine("\n");
-            Console.WriteLine($"Your choice was {userChoice}");
-
-
-
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
-            Console.WriteLine("Program exited");
+            ShowMenu();
         }
     }
 }
