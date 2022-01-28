f8ada0b45ad03b09cccf48c8139881e1a95d5633
Kira Hartlage <kira.hartlage@gmail.com>
2022-01-28 08:00:55 -0500

    reverse string

--- PracticeApp/Program.cs
+++ PracticeApp/Program.cs
@@ -1,9 +1,60 @@
 ﻿using System;
+using System.Text;
 
 namespace PracticeApp
 {
     internal class Program
     {
+        private static void AddTwoNumbers()
+        {
+            Console.WriteLine("You're adding two numbers");
+            Console.WriteLine("Enter the first number");
+            int first = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Enter the second number");
+            int second = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            Console.WriteLine("\n");
+
+            Console.WriteLine($"The sum of {first} and {second} is {first + second}");
+        }
+
+        private static void SubtractTwoNumbers()
+        {
+            Console.WriteLine("You're subtracting two numbers");
+            Console.Write("Enter the first number");
+            int? first = int.Parse(Console.ReadLine());
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            Console.WriteLine("\n");
+
+            Console.Write("Enter the second number");
+            int? second = int.Parse(Console.ReadLine());
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            Console.WriteLine("\n");
+
+            Console.WriteLine($"The sum of {first} and {second} is {first - second}");
+        }
+
+        private static void RemoveWhitespace()
+        {
+
+        }
+
+        private static void ReverseAString()
+        {
+            Console.WriteLine("You're reversing a string");
+            string? phrase = Console.ReadLine();
+            StringBuilder sb = new StringBuilder("", phrase.Length);
+
+            for (int i = phrase.Length - 1; i > 0; i--)
+            {
+                sb.Append(phrase[i]);
+            }
+
+            Console.WriteLine(sb);
+        }
         private static void ShowMenu()
         {
             Console.WriteLine("\n");
@@ -16,6 +67,7 @@ namespace PracticeApp
 
             Console.WriteLine("Enter your choice: ");
             int userChoice = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
 
             Console.WriteLine("\n");
             Console.WriteLine($"Your choice was {userChoice}");
@@ -28,12 +80,18 @@ namespace PracticeApp
                     break;
 
                 case 2:
+                    SubtractTwoNumbers();
+                    ShowMenu();
                     break;
 
                 case 3:
+                    RemoveWhitespace();
+                    ShowMenu();
                     break;
 
                 case 4:
+                    ReverseAString();
+                    ShowMenu():
                     break;
 
                 case 5:
@@ -48,20 +106,6 @@ namespace PracticeApp
                     ShowMenu();
                     break;
             }
-        }
-        private static void AddTwoNumbers()
-        {
-            Console.WriteLine("You're adding two numbers");
-            Console.WriteLine("Enter the first number");
-            int first = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
-            Console.WriteLine("\n");
-
-            Console.WriteLine("Enter the second number");
-            int second = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
-            Console.WriteLine("\n");
-
-            Console.WriteLine($"The sum of {first} and {second} is {first + second}");
-
         }
         static void Main(string[] args)
         {
